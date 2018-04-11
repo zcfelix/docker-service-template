9633d6cce9d17684e6c86f881c42a6eed33d1d9f
Ying Zhou <ybzhou@thoughtworks.com>
2018-04-11 10:27:38 +0800

    finish application.yml and logback.xml

---
+++ src/main/java/com/thoughtworks/felix/dockertemplate/DockerTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.felix.dockertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTemplateApplication.class, args);
+	}
+}
